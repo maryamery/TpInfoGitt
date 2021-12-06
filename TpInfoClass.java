cbc83f49118ccc0219d09f27d6dc00aeb1ace2ac
maryam <maryam.echadi@gmail.com>
2021-12-06 15:33:13 +0100

    commit after jenkins

--- TpInfoClass.java
+++ TpInfoClass.java
@@ -2,6 +2,7 @@
 public class TpInfoClass {
 	public static void main(String args[]) throws Exception{
 		System.out.println("Hello Word");
+		System.out.println("Here we test push to github");
 		System.out.println("Here we test 2 commit");
 	}
 	
