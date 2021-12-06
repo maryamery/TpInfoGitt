18b09ec13217b4a9dc21676f6483da850bae44ce
maryam <maryam.echadi@gmail.com>
2021-12-06 15:30:22 +0100

    first commit

---
+++ TpInfoClass.java
@@ -0,0 +1,8 @@
+
+public class TpInfoClass {
+	public static void main(String args[]) throws Exception{
+		System.out.println("Hello Word");
+		System.out.println("Here we test 2 commit");
+	}
+	
+}
