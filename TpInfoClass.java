68ef49ce4d44a4153c532207142c800106737ac1
maryam <maryam.echadi@gmail.com>
2021-12-06 15:43:35 +0100

    after build

--- TpInfoClass.java
+++ TpInfoClass.java
@@ -3,7 +3,8 @@ public class TpInfoClass {
 	public static void main(String args[]) throws Exception{
 		System.out.println("Hello Word");
 		System.out.println("Here we test push to github");
-		System.out.println("Here we test 2 commit");
+		System.out.println("Here we test 2 commit"); 
+		System.out.println("Here we test JEnkins"); 
 	}
 	
 }
